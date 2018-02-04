2449171b68eaa3ceeb795968c70c93f76feca072
Kang Hyun Woong <cdcd8894@naver.com>
2018-02-04 21:30:57 +0900

    스킬 선택 UI에 기록 + 중복선택 처리

---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Ready/CharacterInfoSkill.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInfoSkill : MonoBehaviour {
+	public List<GameObject> skill;
+	public int skillnum;
+	void OnEnabled()
+	{
+		for (int i = 0; i < 5; ++i)
+		{
+			skill[i].SetActive(false);
+		}
+		skill[Singletone.Instance.Myskill[skillnum]].SetActive(true);
+	}
+}
