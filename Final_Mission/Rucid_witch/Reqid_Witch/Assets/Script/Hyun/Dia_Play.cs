647cf0b88430a8e493aac512eb0d8c4b0e92cdef
wooloves39 <wooloves39@users.noreply.github.com>
2018-02-13 15:30:23 +0900

    날아간것 복구 -ing
    나머지 부분은 집 컴으로 해야함.

---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/Dia_Play.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dia_Play : MonoBehaviour
+{
+	private bool Play = true;
+	public MouseLook mouseLook;
+	public GameObject TalkCanvas;
+	// Use this for initialization
+	void Start()
+	{
+		mouseLook.enabled = false;
+		TalkCanvas.SetActive(false);
+	}
+	public bool getPlay() { return Play; }
+	public void setPlay(bool val) { Play = val; }
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.P)) Play = !Play;
+		if (Play)
+		{
+			mouseLook.enabled = true;
+			TalkCanvas.SetActive(false);
+		}
+		else
+		{
+			mouseLook.enabled = false;
+			TalkCanvas.SetActive(true);
+		}
+	}
+}
