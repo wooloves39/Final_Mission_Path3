97465c8f03f2d6146cc98a35ffec0d8c006c872b
Kang Hyun Woong <cdcd8894@naver.com>
2018-02-18 20:54:01 +0900

    이전 작업 복구

---
+++ Final_Mission/prototype/Assets/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	public Vector3 Var = Vector3.zero;
+	public float Speed = 5.0f;
+	public int cnt = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		
+		this.transform.Rotate (Var*Time.deltaTime * Speed);
+		cnt++;
+		if (cnt > 90) {
+			Var = new Vector3 (getRandom (-90, 90), getRandom (-90, 90), getRandom (-90, 90));
+			cnt = 0;
+		}
+	}
+	int getRandom(int x,int y)
+	{
+		return Random.Range (x, y);
+	}
+}
