0e291eb1276c744d5139ebb36a11f6bd43c8c23b
Kang Hyun Woong <cdcd8894@naver.com>
2018-03-01 13:53:21 +0900

    이펙트 프리팹화
    크기조절도 다시 해둠

---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Effect/MagicBallScale.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicBallScale : MonoBehaviour {
+	public GameObject Ring;
+	public float scale = 1.0f;
+	public float time = 0.05f;
+	public float limit = 3.0f;
+	public float cnt;
+	Vector3 StartRing;
+	Vector3 StartThis;
+	// Use this for initializationvoid OnEnable()
+	void Start()
+	{
+		StartRing = Ring.transform.localScale;
+		StartThis = this.transform.localScale;
+		cnt = 0.0f;
+		StartCoroutine ("ScaleUp");
+	}
+	void OnEnable()
+	{
+		cnt = 0.0f;
+		StartCoroutine ("ScaleUp");
+	}
+	IEnumerator ScaleUp()
+	{
+		while (true) {
+			
+			this.transform.localScale = StartRing + new Vector3 (scale, scale, scale) *cnt/limit;
+			Ring.transform.localScale = StartThis + new Vector3 (scale, scale, scale) *cnt/limit;
+
+			cnt += time;
+			if (limit < cnt)
+				break;
+			else
+				yield return new WaitForSeconds (time);
+		}
+	}
+}
---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Effect/Rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	public Vector3 Var = Vector3.zero;
+	public float Speed = 5.0f;
+	public int cnt = 0;
+	public int limit = 1000;
+
+	// Update is called once per frame
+	void Update () {
+		
+		this.transform.Rotate (Var*Time.deltaTime * Speed);
+		cnt++;
+		if (cnt > limit) {
+			Var = new Vector3 (getRandom (-90, 90), getRandom (-90, 90), getRandom (-90, 90));
+			cnt = 0;
+		}
+	}
+	int getRandom(int x,int y)
+	{
+		return Random.Range (x, y);
+	}
+}
