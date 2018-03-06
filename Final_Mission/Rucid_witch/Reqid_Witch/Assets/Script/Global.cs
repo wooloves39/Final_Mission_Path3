3a90dabbb47c666b547d9cb96b25ae8eb7f4ec20
Kang Hyun Woong <cdcd8894@naver.com>
2018-03-06 22:10:50 +0900

    AI 이동처리 매크로 + 길찾기베이스
    배터리 없어서 나중에 올림
    제발 충돌나지마라

---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Global.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMsg
+{
+	public GameObject obj;
+	public Vector3 destination;
+	public float Speed;
+	public float time;
+};
+public static class Global {
+}
