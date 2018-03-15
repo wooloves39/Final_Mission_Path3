995eef5de8e03b0fe8e276d449f2c316b75cd913
wooloves39 <wooloves39@users.noreply.github.com>
2018-03-16 00:55:02 +0900

    바디 적용, 문제 있음
    로컬포지션 좌표가 이상한데 내일 수정예정

---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/BodyTracking.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyTracking : MonoBehaviour {
+	public GameObject tracker;
+	private Vector3 pos=Vector3.zero;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pos = tracker.transform.position;
+		pos.y = 0;
+		gameObject.transform.position = pos;
+	}
+}
