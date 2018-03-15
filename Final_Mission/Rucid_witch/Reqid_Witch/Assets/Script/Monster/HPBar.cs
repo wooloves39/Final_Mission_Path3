ae8528f07d47c34b59465d9062ba47ed38c98006
Kang Hyun Woong <cdcd8894@naver.com>
2018-03-15 16:51:21 +0900

    프리팹 날라가서 수정 + HP바

---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Monster/HPBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPBar : MonoBehaviour {
+	private ObjectLife life;
+	public GameObject[] Level;
+	private int here;
+
+	// Use this for initialization
+	void Start () {
+		life = GetComponentInParent<ObjectLife>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//this.transform.LookAt(Camera);
+		here = (int)(life.Hp / life.MaxHp * (Level.Length-1));
+		Debug.Log(here);
+		for (int i = 0; i < Level.Length; ++i)
+		{
+			if (i == here)
+				Level[i].SetActive(true);
+			else
+				Level[i].SetActive(false);
+		}
+	}
+}
