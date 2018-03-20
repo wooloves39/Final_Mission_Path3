c7353a28c9be6bb960b08f989b79ef769e7bed1a
FlyBKang <cdcd8894@naver.com>
2018-03-20 18:46:04 +0900

    Tree 갓

---
+++ Final_Mission/prototype/Assets/RotationTree.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTree : MonoBehaviour {
+	public float EulerX = 20.0f;
+	public float EulerY = 20.0f;
+	public void ChildBranch(int n)
+	{
+		this.transform.rotation *= Quaternion.Euler (EulerX * ((n * 2) - 1), EulerY, 0);
+	}
+}
---
+++ Final_Mission/prototype/Assets/Tree.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour {
+	public int NumBranch = 5;
+	public int Max= 5;
+	public int Split = 2;
+	public float Time = 4.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		NumBranch--;
+		if (NumBranch > 0)
+		{
+			StartCoroutine ("PrectalTree");
+		}
+	}
+	IEnumerator PrectalTree()
+	{
+		yield return new WaitForSeconds (Time / (float)Max);
+		for (int i = 0; i < Split; ++i) 
+		{
+			var copy = Instantiate (gameObject);
+		//	copy.tag = ("Branch");
+			var reBranch = copy.GetComponent<Tree> ();
+			reBranch.SendMessage ("ChildBranch", i);
+		}
+	//if(NumBranch == Max-1)
+	//{
+	//	yield return new WaitForSeconds ((float)Max+0.5f);
+	//	GameObject[] temp = GameObject.FindGameObjectsWithTag ("Branch");
+	//	for (int i = 0; i < temp.Length; ++i) {
+	//		temp [i].transform.SetParent (this.transform);
+	//	}
+	//}
+	}
+}
