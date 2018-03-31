6776e6850d3a2735cb05d334da063403331beeee
wooloves39 <wooloves39@users.noreply.github.com>
2018-03-31 15:16:52 +0900

    hand animation 복구 및 디테일 수정

--- Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/Play/HandScript.cs
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/Play/HandScript.cs
@@ -16,75 +16,84 @@ public class HandScript : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		if ((int)myState.GetMyState() < 2)
+		if (InputManager_JHW.LTouchHandleOn()&& InputManager_JHW.LTriggerOn())
 		{
-			if (InputManager_JHW.LTouchHandleOn())
-			{
-				LHandAni.Play("Fist");
-			}
-			else
-			{
-				LHandAni.Play("Idle");
-			}
-			if (InputManager_JHW.RTouchHandleOn())
-			{
-				RHandAni.Play("Fist");
-			}
-			else if (InputManager_JHW.RTriggerOn())
-			{
-				RHandAni.Play("IndexFingerUp");
-			}
-			else
-			{
-				RHandAni.Play("Idle");
-			}
+			LHandAni.Play("Fist");
+
+		}
+		else if (InputManager_JHW.LTouchHandleOn())
+		{
+			LHandAni.Play("restDown");
+		}
+		else if (InputManager_JHW.LTriggerOn())
+		{
+			LHandAni.Play("IndexFingerDown");
+			
 		}
 		else
 		{
 			LHandAni.Play("Idle");
+		}
+		if (InputManager_JHW.RTouchHandleOn() && InputManager_JHW.RTriggerOn())
+		{
+			RHandAni.Play("Fist");
+
+		}
+		else if (InputManager_JHW.RTouchHandleOn())
+		{
+			RHandAni.Play("restDown");
+		}
+		else if (InputManager_JHW.RTriggerOn())
+		{
+			RHandAni.Play("IndexFingerDown");
+
+		}
+		else
+		{
 			RHandAni.Play("Idle");
 		}
-
-		////디버그 상태
-		//if (Input.GetKey(KeyCode.U))
-		//{
-		//	LHandAni.Play("Fist");
-		//	RHandAni.Play("Fist");
-		//}
-		//else if (Input.GetKey(KeyCode.I))
-		//{
-		//	LHandAni.Play("IndexFingerDown");
-		//	RHandAni.Play("IndexFingerDown");
-		//}
-		//else if (Input.GetKey(KeyCode.O))
-		//{
-		//	LHandAni.Play("IndexFingerUp");
-		//	RHandAni.Play("IndexFingerUp");
-		//}
-		//else if (Input.GetKey(KeyCode.P) )
-		//{
-		//	LHandAni.Play("restDown");
-		//	RHandAni.Play("restDown");
-		//}
-		//else if (Input.GetKey(KeyCode.H))
-		//{
-		//	LHandAni.Play("restUp");
-		//	RHandAni.Play("restUp");
-		//}
-		//else if (Input.GetKey(KeyCode.J))
-		//{
-		//	LHandAni.Play("ThumbDown");
-		//	RHandAni.Play("ThumbDown");
-		//}
-		//else if (Input.GetKey(KeyCode.K))
-		//{
-		//	LHandAni.Play("ThumbUp");
-		//	RHandAni.Play("ThumbUp");
-		//}
-		//else
-		//{
-		//	LHandAni.Play("Idle");
-		//	RHandAni.Play("Idle");
-		//}
 	}
+
+	////디버그 상태
+	//if (Input.GetKey(KeyCode.U))
+	//{
+	//	LHandAni.Play("Fist");
+	//	RHandAni.Play("Fist");
+	//}
+	//else if (Input.GetKey(KeyCode.I))
+	//{
+	//	LHandAni.Play("IndexFingerDown");
+	//	RHandAni.Play("IndexFingerDown");
+	//}
+	//else if (Input.GetKey(KeyCode.O))
+	//{
+	//	LHandAni.Play("IndexFingerUp");
+	//	RHandAni.Play("IndexFingerUp");
+	//}
+	//else if (Input.GetKey(KeyCode.P) )
+	//{
+	//	LHandAni.Play("restDown");
+	//	RHandAni.Play("restDown");
+	//}
+	//else if (Input.GetKey(KeyCode.H))
+	//{
+	//	LHandAni.Play("restUp");
+	//	RHandAni.Play("restUp");
+	//}
+	//else if (Input.GetKey(KeyCode.J))
+	//{
+	//	LHandAni.Play("ThumbDown");
+	//	RHandAni.Play("ThumbDown");
+	//}
+	//else if (Input.GetKey(KeyCode.K))
+	//{
+	//	LHandAni.Play("ThumbUp");
+	//	RHandAni.Play("ThumbUp");
+	//}
+	//else
+	//{
+	//	LHandAni.Play("Idle");
+	//	RHandAni.Play("Idle");
+	//}
+
 }
