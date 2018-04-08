f389a7bfd00e2cfaefcf6e321a887e7e9d3b89a6
wooloves39 <wooloves39@users.noreply.github.com>
2018-04-08 21:34:42 +0900

    오브젝트 체인지 적용on
    오브젝트 나오면 바로 적용 가능

---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/Play/ChangeObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeObject : MonoBehaviour {
+	private ParticleSystem particle;
+	// Use this for initialization
+	private void Awake()
+	{
+		particle = GetComponent<ParticleSystem>();
+	}
+	private void OnEnable()
+	{
+		StartCoroutine(changeParticle());
+	}
+	IEnumerator changeParticle()
+	{
+		yield return new WaitForSeconds(2.0f);
+		transform.parent.gameObject.SetActive(false);
+	}
+	// Update is called once per frame
+	void Update () {
+	}
+}
--- Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/Play/SkillChange.cs
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/Play/SkillChange.cs
@@ -7,30 +7,24 @@ public class SkillChange : MonoBehaviour
 	public input_mouse Draw_Rtouch;
 	public Material[] MpMaterial;
 	private Color Color;
+	public GameObject ChangeParticle;
+	public GameObject Hand;
+	public GameObject Seikwan;
+	public GameObject Dell;
+	private Color[] HandColor;
 	// Use this for initialization
 	private void Awake()
 	{
 	}
 	void Start()
 	{
-		switch (input_mouse.curType)
-		{
-			case 0://아즈라
-				Color = new Color(1, 0, 1);
-				break;
-			case 1://세이콴
-				Color = new Color(1, 1, 0);
-				break;
-			case 2://비제
-				Color = new Color(0, 1, 0);
-				break;
-			case 3://베르베시
-				Color = new Color(1, 0, 0);
-				break;
-			case 4://델
-				Color = new Color(0, 1, 1);
-				break;
-		}
+		HandColor = new Color[5];
+		HandColor[0] = new Color(1, 0, 1);
+		HandColor[1] = new Color(1, 1, 0);
+		HandColor[2] = new Color(0, 1, 0);
+		HandColor[3]= new Color(1, 0, 0);
+		HandColor[4] = new Color(0, 1, 1);
+		Color = HandColor[input_mouse.curType];
 		for (int i = 0; i < MpMaterial.Length; ++i)
 			MpMaterial[i].SetColor("_EmissionColor", Color);
 	}
@@ -41,31 +35,34 @@ public class SkillChange : MonoBehaviour
 		if (InputManager_JHW.XButtonDown())
 		{
 			Draw_Rtouch.myType();
-			switch (input_mouse.curType)
-			{
-				case 0://아즈라
-					Color = new Color(1, 0, 1);
-					break;
-				case 1://세이콴
-					Color = new Color(1, 1, 0);
-					break;
-				case 2://비제
-					Color = new Color(0, 1, 0);
-					break;
-				case 3://베르베시
-					Color = new Color(1, 0, 0);
-					break;
-				case 4://델
-					Color = new Color(0, 1, 1);
-					break;
-			}
+			Color = HandColor[input_mouse.curType];
 			for (int i = 0; i < MpMaterial.Length; ++i)
 				MpMaterial[i].SetColor("_EmissionColor", Color);
-
+			ChangeParticle.SetActive(true);
+			StartCoroutine(ChangeObject());
 		}
 	}
 	public Color getMpColor()
 	{
 		return Color;
+	}
+	IEnumerator ChangeObject()
+	{
+		yield return new WaitForSeconds(1.0f);
+		Dell.SetActive(false);
+		Seikwan.SetActive(false);
+		Hand.SetActive(false);
+		if (input_mouse.curType == 1)
+		{
+			Seikwan.SetActive(true);
+		}
+		else if(input_mouse.curType == 4)
+		{
+			Dell.SetActive(true);
+		}
+		else
+		{
+			Hand.SetActive(true);
+		}
 	}
 }
