116f1ae2e7eee77b129fba94159ebadd2c28cbc5
FlyBKang <cdcd8894@naver.com>
2018-04-17 02:04:42 +0900

    Merge

--- Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Effect/RotationTree.cs
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Effect/RotationTree.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class RotationTree : MonoBehaviour {
 	public float EulerX = 20.0f;
 	public float EulerY = 20.0f;
+
+	public float height = 0.8f;
+	public float size = 0.8f;
 	public void ChildBranch(int n)
 	{
 		this.transform.rotation *= Quaternion.Euler (EulerX * ((n * 2) - 1), EulerY, 0);
+		GetComponent<GrowerTree>().X *= height;
+		this.transform.localScale = this.transform.localScale * size;
 	}
 }
