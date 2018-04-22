d31e25370eb51f53facb5ba663de4a71f3694fcd
wooloves39 <wooloves39@users.noreply.github.com>
2018-04-22 23:59:19 +0900

    델, 세이콴 사운드 세팅,
    예비사운드와 UI도 준비됨,

---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/BossSoundSetting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSoundSetting : MonoBehaviour {
+	public enum BosssoundPack { Appearance,Defance, AttackSkill, Die };
+	private BosssoundPack sound;
+	public AudioClip[] sounds;
+	private AudioSource audio;
+	// Use this for initialization
+	void Start () {
+		sound = BosssoundPack.Appearance;
+		audio = GetComponent<AudioSource>();
+	}
+	public void PlayerSound(int soundNum)
+	{
+		if (!audio.isPlaying)
+		{
+			audio.clip = sounds[soundNum];
+			audio.Play();
+		}
+	}
+	public void PlayerSound(BosssoundPack sound)
+	{
+		if (!audio.isPlaying)
+		{
+			audio.clip = sounds[(int)sound];
+			audio.Play();
+		}
+	}
+}
